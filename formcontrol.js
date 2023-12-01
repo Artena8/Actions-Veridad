98d961fc696e83fa2a87c5cdeed12f9b0b38b93d
Héléna Chevalier <lna.chevalier@gmail.com>
2023-12-01 16:04:33 +0100

    ajout / delete form

---
+++ formcontrol.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Nombre de joueurs max
+    var maxChamps = 10;
+
+    $("#addInput").click(function() { //Listener btn add
+        var currentChamps = $("#champ-container").children("input").length; // children(filter) IMPORTANT sinon prend le br aussi
+
+        if (currentChamps < maxChamps) { //control
+            var newInput = $("<input>").attr({ // nom du champ | attr = attributs (name = ??)
+                type: "text",
+                name: "champ" + (currentChamps + 1) // autoincrément
+            });
+
+            $("#champ-container").append(newInput).append("<br>"); //ajout
+        } else {
+            alert("Vous avez atteint le nombre maximal de joueurs.");
+        }
+    });
+
+    $("#deleteInput").click(function() {
+        var currentChamps = $("#champ-container").children("input").length;
+
+        if (currentChamps > 0) {
+            $("#champ-container :last-child").remove();
+            $("#champ-container :last-child").remove();
+        } else {
+            alert("Vous avez atteint le nombre minimal de joueurs.");
+        }
+    });
+    
+});
